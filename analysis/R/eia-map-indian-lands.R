20a18990caa797f11d8a000c9d500616662f4065
Pete Deevakul <editor@mozzarella.website>
2017-04-17 00:30:55 -0400

    add shapefile of federal lands

--- analysis/R/eia-map-indian-lands.R
+++ analysis/R/eia-map-indian-lands.R
@@ -12,17 +12,19 @@ library(mapproj)
 library(rgdal)
 
 # custom themes
-source("00-pd-themes.R")
+source("~/GitHub/standing-rock-research/analysis/R/00-pd-themes.R")
 
-# Map of Indian Lands ---------------------------------------------------------
+# Prepare Map data ------------------------------------------------------------
 
-# READ IN SHAPEFILE
+# READ IN SHAPEFILES: Indian Lands
 native <-  readOGR(dsn = "/Users/pdpd/GitHub/standing-rock-research/data/EIA/indlanp010g.shp_nt00968",
                    layer = "indlanp010g")
 
-summary(native)
+# READ IN SHAPEFILES: Federal Lands
+federal <- readOGR(dsn = "/Users/pdpd/GitHub/standing-rock-research/data/EIA/fedlanp010g.shp_nt00966",
+                   layer = "fedlanp010g")
 
-# create dataframe
+# create dataframe: native lands
 native.map <- fortify(native)
 str(native.map)
 summary(native.map)
@@ -30,14 +32,30 @@ summary(native.map)
 native.map$id <- factor(native.map$id)
 levels(native.map$id)
 
-# map urban population percentage by county
-ggplot(native.map, aes(long, lat, 
-                  group = group)) +
-  geom_polygon(color = "white", size = 0.025) +
+# subset out alaska+hawaii
+native.map <- native.map %>%
+  filter(long > -120)
+
+# create dataframe: federal lands
+fed.map <- fortify(federal)
+summary(fed.map)
+
+fed.map$id <- factor(fed.map$id)
+
+# subset out alaska+hawaii
+fed.map <- fed.map %>%
+  filter(long > -120)
+
+
+# Map of Indian Lands --------------------------------------------------------
+ggplot(native.map, aes(long, lat, group = group)) +
+  geom_polygon(color = "white", fill = "deepskyblue4", size = 0.025) +
+  geom_polygon(data = fed.map,
+               aes(long, lat, group = group),
+               color = "white", fill = "antiquewhite2", size = 0.025) +
   coord_map("polyconic") + 
   pd.theme +
-  theme(legend.position = "right",
-        panel.background = element_blank(),
+  theme(panel.background = element_blank(),
         panel.grid = element_blank(),
         axis.text = element_blank()) +
   guides(fill = guide_legend(reverse = T)) +
